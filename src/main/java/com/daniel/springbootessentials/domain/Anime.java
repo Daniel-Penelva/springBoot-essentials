159e3de71cfb78bad22f75c8779f8dcdc2937b52
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-22 20:45:11 -0300

    first commit

---
+++ src/main/java/com/daniel/springbootessentials/domain/Anime.java
@@ -0,0 +1,13 @@
+package com.daniel.springbootessentials.domain;
+
+public class Anime {
+
+    private String name;
+    
+    public Anime(){
+    }
+
+    public Anime(String name){
+        this.name = name;
+    }
+}
