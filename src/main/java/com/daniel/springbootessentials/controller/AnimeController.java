344baf0c8f7819ae530251e630807615c05e4952
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-22 22:37:17 -0300

    Criando o método findById de anime + alterações no código

--- src/main/java/com/daniel/springbootessentials/controller/AnimeController.java
+++ src/main/java/com/daniel/springbootessentials/controller/AnimeController.java
@@ -1,41 +1,36 @@
 package com.daniel.springbootessentials.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.springbootessentials.domain.Anime;
 import com.daniel.springbootessentials.service.AnimeService;
-import com.daniel.springbootessentials.util.DateUtil;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("animes")
-@Log4j2
+
 @AllArgsConstructor // Lombok - Para injeção de dependência (gera construtor)
 public class AnimeController {
 
-    private DateUtil dateUtil;
     private AnimeService animeService;
 
-    // http://localhost:8080/animes
+    // Listar todos os animes: http://localhost:8080/animes
     @GetMapping
     public List<Anime> list() {
         return animeService.listAll();
     }
 
-    // http://localhost:8080/anime/formatodata
-    @GetMapping(path = "formatodata")
-    private Object formatLocalDateTimeToDatabaseStyle(LocalDateTime now) {
-
-        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-
-        return dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now());
+    // Buscar por id o anime: http://localhost:8080/animes/{id}
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable("id") Long id) {
+        return new ResponseEntity(animeService.findById(id), HttpStatus.OK);
     }
-
 }
--- src/main/java/com/daniel/springbootessentials/service/AnimeService.java
+++ src/main/java/com/daniel/springbootessentials/service/AnimeService.java
@@ -2,7 +2,9 @@ package com.daniel.springbootessentials.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.daniel.springbootessentials.domain.Anime;
 
@@ -10,8 +12,20 @@ import com.daniel.springbootessentials.domain.Anime;
 public class AnimeService {
 
     /* Responsável pela regra de negócio */
+
+    private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Naruto"),
+            new Anime(3L, "Cavalheiros dos Zodíacos"));
+
+    // Buscar todos os animes
     public List<Anime> listAll() {
-		return List.of(new Anime(1L, "DBZ"), new Anime(2L, "Naruto"), new Anime(3L, "Cavalheiros dos Zodíacos"));
-	}
+        return animes;
+    }
+
+    // Buscar por id anime
+    public Anime findById(Long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
 
 }
