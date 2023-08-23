a86fea90d4714f69a1c7eee9d76c42457d97e197
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-22 21:33:57 -0300

    @component +Criando class DateUtil + injeção de dependência com Lombok

--- src/main/java/com/daniel/springbootessentials/controller/AnimeController.java
+++ src/main/java/com/daniel/springbootessentials/controller/AnimeController.java
@@ -1,5 +1,6 @@
 package com.daniel.springbootessentials.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +8,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.springbootessentials.domain.Anime;
+import com.daniel.springbootessentials.util.DateUtil;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("anime/")
+@Log4j2
+@AllArgsConstructor // Lombok - Para injeção de dependência (gera construtor)
 public class AnimeController {
-    
+
+    private DateUtil dateUtil;
+
     // http://localhost:8080/anime/list
-	@GetMapping(path = "list")
-	public List<Anime> list() {
-		return List.of(new Anime("DBZ"), new Anime("Naruto"));
-	}
+    @GetMapping(path = "list")
+    public List<Anime> list() {
+        return List.of(new Anime("DBZ"), new Anime("Naruto"));
+    }
+
+    // http://localhost:8080/anime/formatodata
+    @GetMapping(path = "formatodata")
+    private Object formatLocalDateTimeToDatabaseStyle(LocalDateTime now) {
+
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+
+        return dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now());
+    }
 
 }
