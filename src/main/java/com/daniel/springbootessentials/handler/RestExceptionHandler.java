76ff7a8d0a542847bb8251774fd076317826419d
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-04 23:01:52 -0300

    Sobreescrevendo os métodos do Handler do Spring

--- src/main/java/com/daniel/springbootessentials/handler/RestExceptionHandler.java
+++ src/main/java/com/daniel/springbootessentials/handler/RestExceptionHandler.java
@@ -4,52 +4,76 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.daniel.springbootessentials.exception.BadRequestException;
 import com.daniel.springbootessentials.exception.BadRequestExceptionDetails;
+import com.daniel.springbootessentials.exception.ExceptionDetails;
 import com.daniel.springbootessentials.exception.ValidationExceptionDetails;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException exception) {
-        
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException exception) {
+
         return new ResponseEntity<>(
-            BadRequestExceptionDetails.builder()
-            .timestamp(LocalDateTime.now())
-            .status(HttpStatus.BAD_REQUEST.value())
-            .tittle("Bad Request Exception, Check documentation")
-            .details(exception.getMessage())
-            .developerMessage(exception.getClass().getName())
-            .build(), HttpStatus.BAD_REQUEST);
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .tittle("Bad Request Exception, Check documentation")
+                        .details(exception.getMessage())
+                        .developerMessage(exception.getClass().getName())
+                        .build(),
+                HttpStatus.BAD_REQUEST);
     }
 
-    // Exception MethodArgumentNotValidException - Gera exception se anime for null ou " ".
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception) {
+    // Sobreescrevendo Handler do Spring
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
-        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
 
-         return new ResponseEntity<>(
-            ValidationExceptionDetails.builder()
-            .timestamp(LocalDateTime.now())
-            .status(HttpStatus.BAD_REQUEST.value())
-            .tittle("Bad Request Exception, Check documentation")
-            .details("Check the filed(s) error")
-            .developerMessage(exception.getClass().getName())
-            .field(fields)
-            .fieldMessage(fieldsMessage)
-            .build(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .tittle("Bad Request Exception, Check documentation")
+                        .details("Check the filed(s) error")
+                        .developerMessage(exception.getClass().getName())
+                        .field(fields)
+                        .fieldMessage(fieldsMessage)
+                        .build(),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    // Sobreescrevendo Handler do Spring
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .tittle(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity(exceptionDetails, headers, status);
     }
 }
 
--- src/main/java/com/daniel/springbootessentials/requests/AnimePostRequestBody.java
+++ src/main/java/com/daniel/springbootessentials/requests/AnimePostRequestBody.java
@@ -3,8 +3,6 @@ package com.daniel.springbootessentials.requests;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.URL;
-
 import lombok.Data;
 
 @Data
@@ -13,8 +11,4 @@ public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
     @NotNull(message = "The anime name cannot be null")
     private String name;
-
-    // Para testar a validação de campo (handler) - Mais um campo para testar que vai ser gerado uma lista de Fields
-    @URL(message = "The URL is not valid")
-    private String url;
 }
