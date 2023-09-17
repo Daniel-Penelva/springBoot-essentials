df05f1df9bdbfdbd3b95d8a1c6aa27c9450ac2e9
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-17 20:34:09 -0300

    Criando método teste para listar anime sem paginação (teste de integração) + criando logback-test.xml

--- src/test/java/com/daniel/springbootessentials/integration/AnimeControllerIT.java
+++ src/test/java/com/daniel/springbootessentials/integration/AnimeControllerIT.java
@@ -1,8 +1,13 @@
 package com.daniel.springbootessentials.integration;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,10 +17,14 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.daniel.springbootessentials.domain.Anime;
 import com.daniel.springbootessentials.repository.AnimeRepository;
 import com.daniel.springbootessentials.util.AnimeCreator;
+import com.daniel.springbootessentials.util.AnimePostRequestBodyCreator;
+import com.daniel.springbootessentials.util.AnimePutRequestBodyCreator;
 import com.daniel.springbootessentials.wrapper.PageableResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -46,5 +55,22 @@ public class AnimeControllerIT {
         Assertions.assertThat(animePage).isNotNull();
         Assertions.assertThat(animePage.toList()).isNotEmpty().hasSize(1);
         Assertions.assertThat(animePage.toList().get(0).getName()).isEqualTo(expectedName);
+    }
+    
+    // Teste para listar anime sem paginação
+    @Test
+    @DisplayName("List returns list of anime when successful")
+    void listAll_ReturnsListOfanimes_whenSuccessful() {
+
+        Anime savedAnime = animeRepository.save(AnimeCreator.createAnimeToBeSaved());
+
+        String expectedName = savedAnime.getName();
+
+        List<Anime> animes = testRestTemplate.exchange("/animes/all", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Anime>>() {
+                }).getBody();
+
+        Assertions.assertThat(animes).isNotNull().isNotEmpty().hasSize(1);
+        Assertions.assertThat(animes.get(0).getName()).isEqualTo(expectedName);
     }
 }
